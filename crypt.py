461ee31f2796a3b652253bb0441c49ac711d841d
Kyn <63372753+markyndev@users.noreply.github.com>
2020-08-12 20:42:37 -0300

    Add files via upload

---
+++ crypt.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+nom = datetime.now()
+cryfn = int(nom.strftime("%H"))
+
+alvo  = str(input("$ "))
+cryA = alvo.replace("A", "222");
+cryB = cryA.replace("B", "422");
+cryC = cryB.replace("C", "822");
+cryD = cryC.replace("D", "162");
+cryE = cryD.replace("E", "322");
+cryF = cryE.replace("F", "642");
+cryG = cryF.replace("G", "128");
+cryH = cryG.replace("H", "256");
+cryI = cryH.replace("I", "512");
+cryJ = cryI.replace("J", "102");
+cryK = cryJ.replace("K", "204");
+cryL = cryK.replace("L", "404");
+cryM = cryL.replace("M", "814");
+cryN = cryM.replace("N", "162");
+cryO = cryN.replace("O", "326");
+cryP = cryO.replace("P", "654");
+cryQ = cryP.replace("Q", "132");
+cryR = cryQ.replace("R", "262");
+cryS = cryR.replace("S", "524");
+cryT = cryS.replace("T", "104");
+cryU = cryT.replace("U", "202");
+cryV = cryU.replace("V", "412");
+cryW = cryV.replace("W", "838");
+cryX = cryW.replace("X", "162");
+cryY = cryX.replace("Y", "332");
+cryZ = cryY.replace("Z", "674");
+
+print(cryZ)
+
---
+++ decrypt.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+nom = datetime.now()
+
+alvo  = input("$ ")
+
+cryA = alvo.replace("222", "A");
+cryB = cryA.replace("422", "B");
+cryC = cryB.replace("822", "C");
+
+cryD = cryC.replace("162", "D");
+cryE = cryD.replace("322", "E");
+cryF = cryE.replace("642", "F");
+cryG = cryF.replace("128", "G");
+cryH = cryG.replace("256", "H");
+cryI = cryH.replace("512", "I");
+cryJ = cryI.replace("102", "J");
+cryK = cryJ.replace("204", "K");
+cryL = cryK.replace("404", "L");
+cryM = cryL.replace("814", "M");
+cryN = cryM.replace("162", "N");
+cryO = cryN.replace("326", "O");
+cryP = cryO.replace("654", "P");
+cryQ = cryP.replace("132", "Q");
+cryR = cryQ.replace("262", "R");
+cryS = cryR.replace("524", "S");
+cryT = cryS.replace("104", "T");
+cryU = cryT.replace("202", "U");
+cryV = cryU.replace("412", "V");
+cryW = cryV.replace("838", "W");
+cryX = cryW.replace("162", "X");
+cryY = cryX.replace("332", "Y");
+cryZ = cryY.replace("674", "Z");
+fln = cryZ
+print(cryA)
+print(fln)
+
