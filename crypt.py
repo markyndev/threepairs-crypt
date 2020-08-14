44909f3d867cee9890ab3cb963e3308c603a80fb
Kyn <63372753+markyndev@users.noreply.github.com>
2020-08-14 14:16:34 -0300

    Update crypt.py

--- crypt.py
+++ crypt.py
@@ -1,34 +1,45 @@
 from datetime import datetime
 nom = datetime.now()
-cryfn = int(nom.strftime("%H"))
-
-alvo  = str(input("$ "))
-cryA = alvo.replace("A", "222!");
-cryB = cryA.replace("B", "422@");
-cryC = cryB.replace("C", "822#");
-cryD = cryC.replace("D", "162$");
-cryE = cryD.replace("E", "322%");
-cryF = cryE.replace("F", "642^");
-cryG = cryF.replace("G", "128&");
-cryH = cryG.replace("H", "256*");
-cryI = cryH.replace("I", "512(");
-cryJ = cryI.replace("J", "102)");
-cryK = cryJ.replace("K", "204-");
-cryL = cryK.replace("L", "404_");
-cryM = cryL.replace("M", "814+");
-cryN = cryM.replace("N", "162=");
-cryO = cryN.replace("O", "326|");
-cryP = cryO.replace("P", "654[");
-cryQ = cryP.replace("Q", "132]");
-cryR = cryQ.replace("R", "262{");
-cryS = cryR.replace("S", "524}");
-cryT = cryS.replace("T", "104;");
-cryU = cryT.replace("U", "202:");
-cryV = cryU.replace("V", "412<");
-cryW = cryV.replace("W", "838>");
-cryX = cryW.replace("X", "162.");
-cryY = cryX.replace("Y", "332,");
-cryZ = cryY.replace("Z", "674?");
-crySPC = cryZ.replace(" ", "984~");
-print(crySPC)
+hour = int(nom.strftime("%H"))
+print("")
+print("   ████████ ██████   ██████ ")
+print("      ██    ██   ██ ██      ")
+print("      ██    ██████  ██      ")
+print("      ██    ██      ██      ")
+print("      ██    ██       ██████ ")
+print("")
+alvo  = str(input("Code$: "))
+keyA = alvo.replace("A", "222!");
+keyB = keyA.replace("B", "422@");
+keyC = keyB.replace("C", "822#");
+keyD = keyC.replace("D", "162$");
+keyE = keyD.replace("E", "322%");
+keyF = keyE.replace("F", "642^");
+keyG = keyF.replace("G", "128&");
+keyH = keyG.replace("H", "256*");
+keyI = keyH.replace("I", "512(");
+keyJ = keyI.replace("J", "102)");
+keyK = keyJ.replace("K", "204-");
+keyL = keyK.replace("L", "404_");
+keyM = keyL.replace("M", "814+");
+keyN = keyM.replace("N", "162=");
+keyO = keyN.replace("O", "326|");
+keyP = keyO.replace("P", "654[");
+keyQ = keyP.replace("Q", "132]");
+keyR = keyQ.replace("R", "262{");
+keyS = keyR.replace("S", "524}");
+keyT = keyS.replace("T", "104;");
+keyU = keyT.replace("U", "202:");
+keyV = keyU.replace("V", "412<");
+keyW = keyV.replace("W", "838>");
+keyX = keyW.replace("X", "162.");
+keyY = keyX.replace("Y", "332,");
+keyZ = keyY.replace("Z", "674?");
+keySPC = keyZ.replace(" ", "984~");
+print("")
+print("==============================RESULT=================================")
+print("")
+print(keySPC)
+print("")
+print("=====================================================================")
 
